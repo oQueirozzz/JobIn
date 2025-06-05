fe97de36969b34e9baf6672bd3c06862786fee79
oQueirozzz <renanqsrodrigues@gmail.com>
2025-06-04 21:35:59 -0300

    teste 6

--- api/routes/vagas.js
+++ api/routes/vagas.js
@@ -1,17 +1,16 @@
 import express from 'express';
+import * as vagasController from '../controllers/vagasController.js';
+
 const router = express.Router();
-import vagasController from '../controllers/vagasController.js';
 
 // Rotas públicas
 router.get('/', vagasController.getVagas);
 router.get('/:id', vagasController.getVagaById);
 router.get('/empresa/:empresaId', vagasController.getVagasByEmpresa);
 
-
 // Todas as rotas 
 router.post('/', vagasController.createVaga);
 router.put('/:id', vagasController.updateVaga);
 router.delete('/:id', vagasController.deleteVaga);
-
 
 export default router;
