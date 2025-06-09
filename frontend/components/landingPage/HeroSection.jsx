ac50edeba427ae2d26b91ce2c15c535268f8eabe
GustavoTorelli <gustavo.torelli17@gmail.com>
2025-06-09 16:03:09 -0300

    Feed -> Filtro Front

--- frontend/components/landingPage/HeroSection.jsx
+++ frontend/components/landingPage/HeroSection.jsx
@@ -1,22 +1,46 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 
 export default function HeroSection() {
+  const [empresa, setEmpresa] = useState('');
+  const [vagas, setVagas] = useState([]);
+  const [categoria, setCategoria] = useState('');
+  const [tipo, setTipo] = useState('');
   const router = useRouter();
   const [searchQuery, setSearchQuery] = useState({
-    keyword: '',
-    location: '',
-    area: ''
+    area: '',
+    tipo: '',
+    empresa: ''
   });
 
-  const handleSearch = () => {
-    // Implementar lógica de busca
-    console.log('Searching with:', searchQuery);
-  };
+  
 
+  async function handleSearch() {
+    {
+      try {
+        const queryParams = new URLSearchParams();
+
+        if (searchQuery.area) queryParams.append('area', searchQuery.area);
+        if (searchQuery.tipo) queryParams.append('tipo', searchQuery.tipo);
+        if (searchQuery.empresa) queryParams.append('empresa', searchQuery.empresa);
+
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/vagas?${queryParams}`);
+        const data = await res.json();
+
+        console.log('Resultados:', data);
+
+        window.location.href = '/vagas';
+      } catch (error) {
+        console.error('Erro ao buscar vagas:', error);
+      }
+    };
+
+  
+
+  }
   return (
     <section id="hero" className="relative bg-gradient-to-b from-[#7B2D26]/5 to-white py-20">
       <div className="container mx-auto px-4">
@@ -29,18 +53,26 @@ export default function HeroSection() {
           </p>
 
           <div className="bg-white rounded-xl shadow-lg p-6 mb-8">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
 
 
 
               <div className="relative">
-                <select className="w-full px-4 py-3 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#7B2D26] focus:border-transparent appearance-none bg-white">
+                <select  
+                value={categoria}
+                    onChange={(e) => setCategoria(e.target.value)} 
+                     className="w-full px-4 py-3 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#7B2D26] focus:border-transparent appearance-none bg-white">
                   <option value="">Área</option>
-                  <option value="ti">Tecnologia da Informação</option>
-                  <option value="adm">Administração</option>
-                  <option value="marketing">Marketing</option>
-                  <option value="rh">Recursos Humanos</option>
-                  <option value="financas">Finanças</option>
+                  <option value="TI">Tecnologia da Informação</option>
+                  <option value="Mecânica">Mecânica</option>
+                  <option value="Design">Design</option>
+                  <option value="Administração">Administração</option>
+                  <option value="Marketing">Marketing</option>
+                  <option value="Vendas">Vendas</option>
+                  <option value="Saúde">Saúde</option>
+                  <option value="Educação">Educação</option>
+                  <option value="Engenharia">Engenharia</option>
+                  <option value="Outros">Outros</option>
                 </select>
                 <svg className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400 w-5 h-5 pointer-events-none" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                   <path fill="currentColor" d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"></path>
@@ -48,7 +80,10 @@ export default function HeroSection() {
               </div>
 
               <div className="relative">
-                <select className="w-full px-4 py-3 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#7B2D26] focus:border-transparent appearance-none bg-white">
+                <select  
+                   value={tipo}
+                    onChange={(e) => setTipo(e.target.value)}
+                className="w-full px-4 py-3 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#7B2D26] focus:border-transparent appearance-none bg-white">
                   <option value="">Tipo</option>
                   <option value="presencial">Presencial</option>
                   <option value="hibrido">Híbrido</option>
@@ -59,16 +94,34 @@ export default function HeroSection() {
                 </svg>
               </div>
 
-              
-
+              <div className="relative">
+                <select
+                  className="w-full px-4 py-3 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#7B2D26] focus:border-transparent appearance-none bg-white"
+                  disabled
+                  onChange={(e) => setEmpresa(e.target.value)}
+                
+                >
+                  <option value="">Todas as empresas</option>
+                  {[...new Set(vagas.map(vaga => vaga.nome_empresa))].map((empresaNome) => (
+                    <option key={empresaNome} value={empresaNome} >
+                      {empresaNome}
+                    </option>
+                  ))}
+                </select>
+                <svg className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400 w-5 h-5 pointer-events-none" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                  <path fill="currentColor" d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"></path>
+                </svg>
+              </div>
             </div>
 
             <div className="mt-4 flex justify-center">
-              <a href='/vagas' className="bg-[#7B2D26] text-white px-8 py-3 rounded-full font-medium hover:bg-[#7B2D26]/90 transition-colors duration-300 shadow-lg hover:shadow-xl transform hover:-translate-y-0.5">
+              <button onClick={handleSearch} className="bg-[#7B2D26] cursor-pointer text-white px-8 py-3 rounded-full font-medium hover:bg-[#7B2D26]/90 transition-colors duration-300 shadow-lg hover:shadow-xl transform hover:-translate-y-0.5">
                 Buscar Vagas
-              </a>
+              </button>
             </div>
           </div>
+
+          
 
           <div className="flex flex-col sm:flex-row justify-center gap-4">
             <Link
